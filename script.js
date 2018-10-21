31b7c04718c7f1f6fea7954dabded38353ce5647
nelisthomas <tomnelis53@gmail.com>
2018-10-21 20:01:45 +0200

    sunday night commit

--- script.js
+++ script.js
@@ -30,6 +30,7 @@ document.getElementById("newBattleButton").addEventListener("click", battle);
 document.getElementById("returnButton").addEventListener("click", returnShop);
 
 function battle() {
+    console.log("function battle() started");
 	//PLAYER
 	var player = new Hero("John Doe", 100, 100, 10, 1, 0);
 	//ENEMIES
@@ -49,60 +50,99 @@ function battle() {
 
     var randomEnemy = enemies[Math.floor(Math.random() * enemies.length)];
     displayStats(player, randomEnemy);
-    xpCheck();
+    
+    // xpCheck();
 
     function main() {
-        attack(player, randomEnemy)
-    }
-    function attack (player, enemy) {
-        // if (enemy.health && player.health >= 0) {
-            enemy.health -= player.damage;
-            displayStats(player, randomEnemy);
-            var y = setTimeout(z, 800);
-                function z(){
-            if (enemy.health < 1) {
-                document.getElementById("enemyHealthBar").style.display = "none";
-                document.getElementById("battleButton").style.display = "none";
-                document.getElementById("returnButton").style.display = "block";
-                displayStats(player, randomEnemy);
-                var x = setTimeout(deadEnemy, 10);
-                function deadEnemy() {
-                    alert( randomEnemy.name + " has been defeated!");
+        console.log("function main() started");
+        var y = true;
+        if (y = true){
+
+        function attack (player, enemy) {
+            console.log("function attack (player, enemy) started");
+            // if (enemy.health && player.health >= 0) {
+                function damageEnemy(player, enemy){
+                    console.log("function damageEnemy(player, enemy) started");
+                    var dmg = player.damage;
+                    var bar = document.getElementById("enemyHealthBar");
+                    var startHealth = enemy.health;
+                    var endHealth = enemy.health - player.damage;
+                    var x = setInterval(dmg, 500);
+                    function dmg(){
+                        while(enemy.health != endHealth){
+                            if (enemy.health == endHealth){
+                                clearInterval(x);
+                            } else {
+                                enemy.health -= 1;
+                                bar.style.width = bar.style.width + 1;
+                            }
+                        }   
+                        // for (i = 0; i < dmg; i++){
+                        //  var x = setTimeout(subtractHealth, 1000);
+                        //  function subtractHealth(){
+                        //  enemy.health -= 1;
+                        //  displayStats(player,randomEnemy);    
+                        //  }
+                     }
+                     console.log("function damageEnemy(player, enemy) ended");
+                    }
+                    
                 }
-            } else{
-                    player.health -= enemy.damage;
-                }
+                // attack(player, randomEnemy);
+                // console.log("attack(player, randomEnemy) called");
                 displayStats(player, randomEnemy);
-                if (player.health < 1) {
+                
+                var y = setTimeout(z, 800);
+                    function z(){
+                if (randomEnemy.health < 1) {
                     document.getElementById("enemyHealthBar").style.display = "none";
                     document.getElementById("battleButton").style.display = "none";
                     document.getElementById("returnButton").style.display = "block";
                     displayStats(player, randomEnemy);
-                    var x = setTimeout(deadPlayer, 10);
-                    function deadPlayer() {
-                        alert(player.name + " has died like a little bitch!");
+                    
+                    var x = setTimeout(deadEnemy, 10);
+                    function deadEnemy() {
+                        alert( randomEnemy.name + " has been defeated!");
+                    }
+                } else{
+                        player.health -= randomEnemy.damage;
+                    }
+                    displayStats(player, randomEnemy);
+                    
+                    if (player.health < 1) {
+                        document.getElementById("enemyHealthBar").style.display = "none";
+                        document.getElementById("battleButton").style.display = "none";
+                        document.getElementById("returnButton").style.display = "block";
+                        displayStats(player, randomEnemy);
+                        
+                        var x = setTimeout(deadPlayer, 10);
+                        function deadPlayer() {
+                            alert(player.name + " has perished to a " + randomEnemy.name);
+                        }
                     }
                 }
-            }
-        //}
-        
-
+        y = false;
+    } else {
+        return;
     }
-
+    console.log("function main() ended");
+}
+console.log("function battle() ended")
+};
     
 
-	function xpCheck() {
-    if (player.xp >= 100) {
-        player.level++;
-        player.xp -= 100;
-    }
-}
-};
+	// function xpCheck() {
+ //    if (player.xp >= 100) {
+ //        player.level++;
+ //        player.xp -= 100;
+ //    }
+
 
 
 
 
 function displayStats(player, enemy) {
+    console.log("function displayStats(player, enemy) started");
     //UPDATE PLAYER STATS
     document.getElementById("playerHeader").innerHTML = player.name + ":";
     document.getElementById("playerHealth").innerHTML = "Health: " + player.health + "/" + player.maxHealth;
@@ -113,6 +153,7 @@ function displayStats(player, enemy) {
     //UPDATE ENEMY STATS
     document.getElementById("enemyHeader").innerHTML = enemy.name + ":";
     document.getElementById("enemyHealth").innerHTML = "Health: " + enemy.health;
+    //ENEMY HEALTHBAR
     var enemyHealthPercentage = (enemy.health / enemy.maxHealth) * 100;
     document.getElementById("enemyHealthBar").style.width = enemyHealthPercentage + "%";
     document.getElementById("enemyHealthBar").style.display = "block";
